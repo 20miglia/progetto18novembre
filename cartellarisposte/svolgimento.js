f0d1f75ec5ef9e909d890d04d67a2c4c08405a3e
Gianluca Ventimiglia <20migliagianluca@gmail.com>
2024-11-21 13:46:59 +0100

    Modifica168169

--- cartellarisposte/svolgimento.js
+++ cartellarisposte/svolgimento.js
@@ -165,10 +165,10 @@ for (let i = 1; i <= 100; i++) {
       console.log("Buzz")
     } else if ((i % 3 === 0) && (i % 5 === 0)) {
       console.log("FizzBuzz")
+    } else {
+      console.log(i)
     }
   }
-
-
 
 
 
